36114615ba25993fdaf5ccd6d25ed919abc66d89
Junior Egger <junior.egger@gmail.com>
2022-07-15 18:50:38 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/workshop/entities/Category.java
+++ src/main/java/com/educandoweb/workshop/entities/Category.java
@@ -1,5 +1,7 @@
 package com.educandoweb.workshop.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
@@ -23,7 +25,8 @@ public class Category implements Serializable {
 
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
